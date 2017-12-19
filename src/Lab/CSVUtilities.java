7a644da18b6ba5f6c11c7295ad8d46e663517ed2
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2017-12-19 13:12:58 -0500

    aaaaaaa

---
+++ src/Lab/CSVUtilities.java
@@ -0,0 +1,45 @@
+package Lab;
+
+import java.io.*;
+import java.util.*;
+
+public class CSVUtilities {
+	private ArrayList<String> CSVData = new ArrayList<String>();
+	private int numColumns;
+	public static void main(String[] args) {
+		CSVParser("C:\\Users\\BT_1N3_18\\eclipse-workspace\\SKRRRRRRRRRRRRRRRRRRRRRRRRRRT\\CSVUtil\\src\\Lab\\yes.csv");
+	}
+	
+	public static void CSVParser(String csvFile){
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                System.out.println(line);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+	
+	
+}
